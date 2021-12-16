47f0d1551bb36d422580fb24d54827cdd629f655
Jesper Ripnäs <jepperipnas@gmail.com>
2021-12-16 10:06:01 +0100

    Removed unused chars in publication date value to make it pretty

--- src/components/common/jobcard.jsx
+++ src/components/common/jobcard.jsx
@@ -2,12 +2,14 @@ import "./css/jobcard.css"
 import { Link } from 'react-router-dom';
 
 const JobCard = (props) => {
+    const publicationString = props.job.publication_date.replace(/T|Z/g, " ");
+
     return (
         <div className="Card">
             <div className="CardTitle"><Link to={props.job.id}>{props.job.headline}</Link></div>
-            <div className="CardDescription">{props.job.description === null ? null : null}</div>
+            <div className="CardDescription">{props.job.description.text === null ? null : null}</div>
             <div className="CardDescription">{props.job.ort}</div>
-            <div className="CardPublicationDate">Skapad: {props.job.publication_date}</div>
+            <div className="CardPublicationDate">Skapad: {publicationString}</div>
         </div>
     )
 } 
