2eed23b3120ffeb8092d06e8a2830690d152bb6c
Jesper Ripnäs <jepperipnas@gmail.com>
2022-01-06 01:13:33 +0100

    Added new component that shows all requests to backend from clients

---
+++ src/components/public/Logger.tsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+import { Row, Col, Container } from "react-bootstrap";
+import Table from 'react-bootstrap/Table'
+
+const Logger = () => {
+  const [logApiData, SetLogApiData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const API = "http://82.102.1.109/api/joblistings/searchhistory";
+      await fetch(API)
+        .then((response) => response.json())
+        .then((data) => SetLogApiData(data))
+        .catch((err) => console.error(err));
+    };
+    fetchData();
+  }, []);
+  return (
+    <>
+    <Container style={{
+      fontSize: "12px",
+      width: "60%"
+    }}>
+    <Table striped bordered hover variant="light" size="sm" responsive="sm"> 
+    <thead>
+    <tr>
+      <th style={{width:"12rem"}}># ID</th>
+      <th style={{width:"11rem"}}>Date</th>
+      <th>Search word</th>
+    </tr>
+  </thead>
+  <tbody>
+  {logApiData.slice().reverse().map((log: any) => (
+      <tr>
+        <th>{log.id}</th>
+        <th>{log.timeStamp.replace(/T|Z/g, " ")}</th>
+        <th>{log.searchstring}</th>
+      </tr>
+      ))}
+  </tbody>
+    </Table>
+    </Container>
+    </>
+  );
+};
+export default Logger;
