d9d03e924a596b2c35ac9d9f8cfce88c919843ff
Robin Ferm <robinferm@live.com>
2022-01-13 16:15:10 +0100

    create query string from ids and fetch ad-data. Show headline instead of id in saved ads

--- src/components/common/SavedAds.tsx
+++ src/components/common/SavedAds.tsx
@@ -2,19 +2,40 @@ import { useState, useEffect } from "react";
 import { Row, Col, Container } from "react-bootstrap";
 
 const SavedAds = (props: any) => {
-    const [savedAds, setSavedAds] = useState([]);
+    const [queryString, setQueryString] = useState();
+    const [savedAdsData, setSavedAdsData] = useState([])
+
     const getSavedAds = () => {
+        // Get ad ids from local storage
         var existingEntries = JSON.parse(localStorage.getItem("allEntries"));
         if(existingEntries !== null){
-            setSavedAds(existingEntries);
+
+            // Create query string
+            var ids = existingEntries.map((id: string) => (
+                id + '&ids='
+            )).join('');
+
+            setQueryString(ids);
         }
     };
 
+
+    const getAdsData = () => {
+        if (queryString !== undefined){
+            const API = "http://localhost/api/joblistings/?ids=" + queryString;
+            fetch(API)
+            .then((response) => response.json())
+            .then((data) => setSavedAdsData(data))
+            .catch((err) => console.error(err));
+        }
+    };
+    
     useEffect(() => getSavedAds(), [props.savedAdCounter]);
 
+    useEffect(() => getAdsData(), [queryString])
     return(
-        <div>{savedAds === undefined ? null : savedAds.map((job: any) => (
-                <div key={job}>{job}</div>
+        <div>{queryString === undefined ? null : savedAdsData.map((job: any) => (
+                <div key={job.id} onClick={() =>console.log(job.headline)}>{job.headline}</div>
         ))}</div>
     )
     };
