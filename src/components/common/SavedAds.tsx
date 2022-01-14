1b57b5a73856a1bdbef461aa6939aae5215b0e01
Jesper Ripnäs <jepperipnas@gmail.com>
2022-01-14 13:59:07 +0100

    fixed compiling errors

--- src/components/common/SavedAds.tsx
+++ src/components/common/SavedAds.tsx
@@ -2,41 +2,43 @@ import { useState, useEffect } from "react";
 import { Row, Col, Container } from "react-bootstrap";
 
 const SavedAds = (props: any) => {
-    const [queryString, setQueryString] = useState();
-    const [savedAdsData, setSavedAdsData] = useState([])
+  const [queryString, setQueryString] = useState("");
+  const [savedAdsData, setSavedAdsData] = useState([]);
 
-    const getSavedAds = () => {
-        // Get ad ids from local storage
-        var existingEntries = JSON.parse(localStorage.getItem("allEntries"));
-        if(existingEntries !== null){
+  const getSavedAds = () => {
+    // Get ad ids from local storage
+    var existingEntries = JSON.parse(localStorage.getItem("allEntries") || '{}');
+    if (existingEntries !== null) {
+      // Create query string
+      var ids = existingEntries.map((id: string) => id + "&ids=").join("");
 
-            // Create query string
-            var ids = existingEntries.map((id: string) => (
-                id + '&ids='
-            )).join('');
+      setQueryString(ids);
+    }
+  };
 
-            setQueryString(ids);
-        }
-    };
+  const getAdsData = () => {
+    if (queryString !== undefined) {
+      const API = "http://localhost/api/joblistings/?ids=" + queryString;
+      fetch(API)
+        .then((response) => response.json())
+        .then((data) => setSavedAdsData(data))
+        .catch((err) => console.error(err));
+    }
+  };
 
+  useEffect(() => getSavedAds(), [props.savedAdCounter]);
 
-    const getAdsData = () => {
-        if (queryString !== undefined){
-            const API = "http://localhost/api/joblistings/?ids=" + queryString;
-            fetch(API)
-            .then((response) => response.json())
-            .then((data) => setSavedAdsData(data))
-            .catch((err) => console.error(err));
-        }
-    };
-    
-    useEffect(() => getSavedAds(), [props.savedAdCounter]);
-
-    useEffect(() => getAdsData(), [queryString])
-    return(
-        <div>{queryString === undefined ? null : savedAdsData.map((job: any) => (
-                <div key={job.id} onClick={() =>console.log(job.headline)}>{job.headline}</div>
-        ))}</div>
-    )
-    };
-  export default SavedAds;
+  useEffect(() => getAdsData(), [queryString]);
+  return (
+    <div>
+      {queryString === null 
+        ? null
+        : savedAdsData.map((job: any) => (
+            <div key={job.id} onClick={() => console.log(job.headline)}>
+              {job.headline}
+            </div>
+          ))}
+    </div>
+  );
+};
+export default SavedAds;
